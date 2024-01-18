74ecc6a36da46e4c603afacde5c1b68fe09a3962
bhagya199019 <117078616+bhagya199019@users.noreply.github.com>
2024-01-18 10:34:22 +0530

    Update WebConfiguration.java

--- src/main/java/com/example/demo/configuration/WebConfiguration.java
+++ src/main/java/com/example/demo/configuration/WebConfiguration.java
@@ -11,47 +11,15 @@ import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import java.util.Arrays;
 
+
 @Configuration
 @EnableWebMvc
 public class WebConfiguration {
-	 
+
     private static final Long MAX_AGE = 3600L;
     private static final int CORS_FILTER_ORDER = -102;
- 
-//     @Bean
-//     public CorsFilter corsFilter() {
-//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//         CorsConfiguration config = new CorsConfiguration();
-//         config.setAllowCredentials(true);
-//      //   config.addAllowedOrigin("http://localhost:3000");
-// 	// config.addAllowedOrigin("https://career-flow-froentend.vercel.app");
-// 	     config.addAllowedOrigin("https://user-management-system-frontend-iota.vercel.app");
-//         config.setAllowedHeaders(Arrays.asList(
-//                 HttpHeaders.AUTHORIZATION,
-//                 HttpHeaders.CONTENT_TYPE,
-//                 HttpHeaders.ACCEPT));
-//         config.setAllowedMethods(Arrays.asList(
-//                 HttpMethod.GET.name(),
-//                 HttpMethod.POST.name(),
-//                 HttpMethod.PUT.name(),
-//                 HttpMethod.DELETE.name()));
-//         config.setMaxAge(MAX_AGE);
-//         source.registerCorsConfiguration("/**", config);
-//         //FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-//         /*CorsFilter bean = new CorsFilter(source);
- 
-//         // should be set order to -100 because we need to CorsFilter before SpringSecurityFilter
-//         bean.setOrder(CORS_FILTER_ORDER);
-//         return bean;*/
-//         CorsFilter corsFilter = new CorsFilter(source);
 
-//         // Set the order to CORS_FILTER_ORDER
-//         FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter);
-//         bean.setOrder(CORS_FILTER_ORDER);
-//         return corsFilter;
-//     }
-// }
-	 @Bean
+    @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
@@ -70,8 +38,12 @@ public class WebConfiguration {
 
         source.registerCorsConfiguration("/**", config);
 
-        // CorsFilter bean = new CorsFilter(source);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        return new CorsFilter(source);
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistrationBean() {
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter());
         bean.setOrder(CORS_FILTER_ORDER);
         return bean;
     }
